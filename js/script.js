77894565869d72ce22296b4b237e93e05203204d
meobeoo <hoaphamduc2399@gmail.com>
2024-04-17 17:19:32 +0700

    update

--- js/script.js
+++ js/script.js
@@ -1,31 +1,49 @@
 function getRandomQuote() {
-    document.getElementById('quote').textContent = '';
-    document.getElementById('author').textContent = '';
-  
-    fetch('../data/quote.json')
-      .then(response => response.json())
-      .then(data => {
-        const randomIndex = Math.floor(Math.random() * data.length);
-        const randomQuote = data[randomIndex].quote;
-        const randomAuthor = data[randomIndex].author;
-        
-        animateSpan('quote', randomQuote);
-        animateSpan('author', randomAuthor);
-      })
-      .catch(error => {
-        console.error('Đã xảy ra lỗi:', error);
-    });
+  document.getElementById('quote').textContent = '';
+  document.getElementById('author').textContent = '';
+
+  const button = document.getElementById('getRandomQuoteButton');
+  button.disabled = true;
+
+  fetch('../data/quote.json')
+    .then(response => response.json())
+    .then(data => {
+      const randomIndex = Math.floor(Math.random() * data.length);
+      const randomQuote = data[randomIndex].quote;
+      const randomAuthor = data[randomIndex].author;
+      
+      animateSpan('quote', randomQuote, () => {
+          animateSpan('author', randomAuthor, () => {
+              button.disabled = false;
+          });
+      });
+    })
+    .catch(error => {
+      console.error('Đã xảy ra lỗi:', error);
+      button.disabled = false;
+  });
 }
 
-function animateSpan(spanId, text) {
-    const span = document.getElementById(spanId);
-    let index = 0;
-    const interval = setInterval(() => {
-        if (index < text.length) {
-        span.textContent += text[index];
-        index++;
-        } else {
-        clearInterval(interval);
-        }
-    }, 50);
+function animateSpan(spanId, text, callback) {
+  const span = document.getElementById(spanId);
+  let index = 0;
+  const interval = setInterval(() => {
+      if (index < text.length) {
+      span.textContent += text[index];
+      index++;
+      } else {
+      clearInterval(interval);
+      if (callback) {
+          callback();
+      }
+      }
+  }, 50);
 }
+
+window.onload = function() {
+  function lockScroll() {
+      window.scrollTo(0, 0);
+  }
+  window.addEventListener('resize', lockScroll);
+  window.addEventListener('load', lockScroll);
+};
