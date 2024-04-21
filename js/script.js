89582419de03e507661483408e867e16ced446d2
hoaphamduc <hoaphamduc2399@gmail.com>
2024-04-22 04:00:26 +0700

    update

--- js/script.js
+++ js/script.js
@@ -8,7 +8,6 @@ function getRandomQuote() {
   fetch('../data/quote.json')
     .then(response => response.json())
     .then(data => {
-      document.getElementById('captureButton').style.display = 'none';
       const randomIndex = Math.floor(Math.random() * data.length);
       const randomQuote = data[randomIndex].quote;
       const randomAuthor = data[randomIndex].author;
@@ -16,7 +15,6 @@ function getRandomQuote() {
       animateSpan('quote', randomQuote, () => {
           animateSpan('author', randomAuthor, () => {
               button.disabled = false;
-              document.getElementById('captureButton').style.display = 'block';
           });
       });
     })
@@ -61,38 +59,5 @@ function openSourceCode() {
   if (confirmation) {
     var url = 'https://github.com/hoaphamduc/LoveMessage';
     window.open(url, '_blank');
-  }
-}
-
-async function captureScreen() {
-  try {
-    const stream = await navigator.mediaDevices.getDisplayMedia({ preferCurrentTab: true });
-    const video = document.createElement("video");
-
-    video.addEventListener("loadedmetadata", async () => {
-      const canvas = document.createElement("canvas");
-      const ctx = canvas.getContext("2d");
-
-      canvas.width = video.videoWidth;
-      canvas.height = video.videoHeight;
-
-      video.play();
-      ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
-      stream.getVideoTracks()[0].stop();
-
-      const imageDataURL = canvas.toDataURL('image/png');
-      const blobData = await (await fetch(imageDataURL)).blob();
-
-      var now = new Date();
-      var timestamp = now.getHours() + '-' + now.getMinutes() + '-' + now.getSeconds() + '_' + now.getDate() + '-' + (now.getMonth() + 1) + '-' + now.getFullYear();
-
-      const link = document.createElement('a');
-      link.href = URL.createObjectURL(blobData);
-      link.download = 'LoveMessage_' + timestamp + '.png';
-      link.click();
-    });
-    video.srcObject = stream;
-  } catch (error) {
-    console.log(error);
   }
 }
